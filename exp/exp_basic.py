90eeb652fb20f8616dec2d7ebdd50d8c1db9d30c
rosemike23 <122757143+rosemike23@users.noreply.github.com>
2024-07-06 16:31:33 +0800

    Add files via upload

---
+++ exp/exp_basic.py
@@ -0,0 +1,58 @@
+import os
+from models import TimesNet
+# from models import TimesWave
+# from models import DLinear
+# from models import TimesWaveReal
+# from models import TimesWaveBatchAfter
+# from models import TimesWave0
+# from models import TimesWave_norm
+# from models import TimesWave_onlywt
+# from models import TimesWave_kernal
+from models import TimesWave_boost
+# from models import Dim
+
+import torch
+from models import Autoformer, DLinear, FEDformer, Informer
+
+
+class Exp_Basic(object):
+    def __init__(self, args):
+        self.args = args
+        self.model_dict = {
+            'TimesNet': TimesNet,
+            'Autoformer': Autoformer,
+            'DLinear': DLinear,
+            'FEDformer': FEDformer,
+            'Informer': Informer,
+            # 'TimesWave': TimesWave,
+            'TimesWave_boost': TimesWave_boost,
+        }
+        self.device = self._acquire_device()
+        self.model = self._build_model().to(self.device)
+
+    def _build_model(self):
+        raise NotImplementedError
+        return None
+
+    def _acquire_device(self):
+        if self.args.use_gpu:
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(
+                self.args.gpu) if not self.args.use_multi_gpu else self.args.devices
+            device = torch.device('cuda:{}'.format(self.args.gpu))
+            print('Use GPU: cuda:{}'.format(self.args.gpu))
+        else:
+            device = torch.device('cpu')
+            print('Use CPU')
+        return device
+
+    def _get_data(self):
+        pass
+
+    def vali(self):
+        pass
+
+    def train(self):
+        pass
+
+    def test(self):
+        pass
